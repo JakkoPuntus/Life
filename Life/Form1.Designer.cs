9c93a51a00a252e7ca5624d0ac3e0e31f8067feb
puntusiche@protonmail.com <puntusiche@protonmail.com>
2020-04-15 23:25:01 +0300

    Project created

---
+++ Life/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace Life
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.buttonPlay = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Black;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.ErrorImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.ErrorImage")));
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 33);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(64, 64);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // buttonPlay
+            // 
+            this.buttonPlay.BackColor = System.Drawing.Color.PaleGreen;
+            this.buttonPlay.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonPlay.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.buttonPlay.ForeColor = System.Drawing.Color.White;
+            this.buttonPlay.Location = new System.Drawing.Point(13, 454);
+            this.buttonPlay.Name = "buttonPlay";
+            this.buttonPlay.Size = new System.Drawing.Size(121, 47);
+            this.buttonPlay.TabIndex = 1;
+            this.buttonPlay.Text = "Play";
+            this.buttonPlay.UseVisualStyleBackColor = false;
+            this.buttonPlay.Click += new System.EventHandler(this.buttonPlay_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.BackColor = System.Drawing.Color.PaleGreen;
+            this.buttonClear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.buttonClear.ForeColor = System.Drawing.Color.White;
+            this.buttonClear.Location = new System.Drawing.Point(306, 454);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(121, 47);
+            this.buttonClear.TabIndex = 1;
+            this.buttonClear.Text = "Clear";
+            this.buttonClear.UseVisualStyleBackColor = false;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 2;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Puntus\'s Life";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Crimson;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(381, 0);
+            this.button1.Margin = new System.Windows.Forms.Padding(0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(58, 27);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(439, 27);
+            this.panel1.TabIndex = 4;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Black;
+            this.pictureBox2.Location = new System.Drawing.Point(95, 137);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(258, 258);
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(439, 513);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonPlay);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button buttonPlay;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ Life/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Life
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int[,] pixels = new int[439, 513];
+        int[,] pixelsNext = new int[439, 513];
+        Pen myPen = new Pen(Color.White, 0.5F);
+        Pen blackPen = new Pen(Color.Black, 0.5F);
+        Graphics graphics;
+        Graphics g;
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                g = Graphics.FromHwnd(pictureBox2.Handle);
+                graphics = Graphics.FromHwnd(pictureBox1.Handle);
+                graphics.DrawRectangle(myPen, e.X, e.Y, 0.5F, 0.5F);
+                pixels[e.X, e.Y] = 1;
+            }
+            label1.BackColor = panel1.BackColor;
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                graphics = Graphics.FromHwnd(pictureBox1.Handle);
+                graphics.DrawRectangle(myPen, e.X, e.Y, 0.5F, 0.5F);
+                pixels[e.X, e.Y] = 1;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int x = 0; x < 438; x++)
+            {
+                for (int y = 0; y < 512; y++)
+                {
+                    pixels[x, y] = 0;
+                    pixelsNext[x, y] = 0;
+                }
+            }
+        }
+
+
+        int countOfNeightbours = 0;
+        private void buttonPlay_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            timer1.Start();
+            
+
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            for (int x = 0; x < 439; x++)
+            {
+                for (int y = 0; y < 513; y++)
+                {
+                    pixels[x, y] = 0;
+                    pixelsNext[x, y] = 0;
+                }
+            }
+            timer1.Stop();
+            timer1.Enabled = false;
+            // Create solid brush.
+           
+
+            // Fill rectangle to screen.
+            graphics.FillRectangle(blackBrush, 0, 0, 329, 513);
+
+        }
+
+        SolidBrush blackBrush = new SolidBrush(Color.Black);
+        SolidBrush whiteBrush = new SolidBrush(Color.White);
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            for (int x = 64; --x >= 1;)
+            {
+                for (int y = 64; --y >= 1;)
+                {
+                    countOfNeightbours = 0;
+                    if (pixels[x, y] == 1)
+                    {
+                        if (pixels[x - 1, y + 1] == 1)
+                            countOfNeightbours++;
+
+                        if (pixels[x, y + 1] == 1)
+                            countOfNeightbours++;
+                        if (pixels[x + 1, y + 1] == 1)
+                            countOfNeightbours++;
+
+                        if (pixels[x - 1, y] == 1)
+                            countOfNeightbours++;
+                        if (pixels[x + 1, y] == 1)
+                            countOfNeightbours++;
+
+                        if (pixels[x - 1, y - 1] == 1)
+                            countOfNeightbours++;
+                        if (pixels[x, y - 1] == 1)
+                            countOfNeightbours++;
+                        if (pixels[x + 1, y - 1] == 1)
+                            countOfNeightbours++;
+
+
+                        if (countOfNeightbours < 2 || countOfNeightbours > 3)
+                            pixelsNext[x, y] = 0;
+                        else
+                            pixelsNext[x, y] = 1;
+                    }
+                    else if (pixels[x, y] == 0)
+                    {
+                        
+                        if (pixels[x - 1, y + 1] == 1)
+                            countOfNeightbours++;
+
+                        if (pixels[x, y + 1] == 1)
+                            countOfNeightbours++;
+                        if (pixels[x + 1, y + 1] == 1)
+                            countOfNeightbours++;
+
+                        if (pixels[x - 1, y] == 1)
+                            countOfNeightbours++;
+                        if (pixels[x + 1, y] == 1)
+                            countOfNeightbours++;
+
+                        if (pixels[x - 1, y - 1] == 1)
+                            countOfNeightbours++;
+                        if (pixels[x, y - 1] == 1)
+                            countOfNeightbours++;
+                        if (pixels[x + 1, y - 1] == 1)
+                            countOfNeightbours++;
+
+                        if (countOfNeightbours == 3)
+                            pixelsNext[x, y] = 1;
+                        else
+                            pixelsNext[x, y] = 0;
+                    }
+                }
+            }
+            RedrawField();
+        }
+
+        private void RedrawField()
+        {
+            for (int x = 64; --x >= 1;)
+            {
+                for (int y = 64; --y >= 1;)
+                {
+                    if (pixelsNext[x, y] == 0)
+                    {
+                        graphics.DrawRectangle(blackPen, x, y, 0.5f, 0.5f);
+                        g.FillRectangle(blackBrush, x, y, 4, 4);
+                        pixels[x, y] = 0;
+                        
+                    }
+                    else
+                    {
+                        
+                        graphics.DrawRectangle(myPen, x, y, 0.5f, 0.5f);
+                        g.FillRectangle(whiteBrush, x, y, 4, 4);
+                        pixels[x, y] = 1;
+                    }
+                }
+            }
+        }
+
+        Point lastPoint;
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - lastPoint.X;
+                this.Top += e.Y - lastPoint.Y;
+
+            }
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            lastPoint = new Point(e.X, e.Y);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+        }
+    }
+}
+
